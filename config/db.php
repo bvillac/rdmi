d04851eb82889ed69e765ea43041edb0702ab2c0
blvillac <byronvillacreses@gmail.com>
2016-11-30 23:49:24 -0500

    datos de acceso

--- config/db.php
+++ config/db.php
@@ -3,10 +3,12 @@
 return [
     //'class' => 'yii\db\Connection',
     'class' => 'app\components\CConnection',
-    'dsn'   => 'mysql:host=localhost;dbname=rdmi',
+    'dsn'   => 'mysql:host=192.168.10.100;dbname=rdmi',
+    //'dsn'   => 'mysql:host=localhost;dbname=rdmi',
     'username' => 'rdmiuser',
     'password' => 'rdmiuser2016',
     'charset'  => 'utf8',
     'dbname'   => 'rdmi',
-    'dbserver' => 'localhost'
+    //'dbserver' => 'localhost'
+    'dbserver' => '192.168.10.100'
 ];
