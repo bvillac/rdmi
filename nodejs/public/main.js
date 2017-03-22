689e86bfd28bebdcda4332c46c6893529790a04a
root <root@prueba.utimpor>
2017-03-21 19:48:20 -0500

    DATA 1

--- nodejs/public/main.js
+++ nodejs/public/main.js
@@ -6,8 +6,8 @@
 
 $( document ).ready(function() {
 
-    var socket = io.connect('http://192.168.10.100:8890');
-    //var socket = io.connect('http://192.168.10.156:8890');
+    //var socket = io.connect('http://192.168.10.100:8890');
+    var socket = io.connect('http://192.168.10.156:8890');
     //var socket = io.connect('http://localhost:8890');
 
     socket.on('notification', function (data) {
