3cd7787418814c864ac031e833e409f3fb98d46e
root <root@vs.server>
2017-09-08 07:52:45 -0500

    Portada

--- views/site/index.php
+++ views/site/index.php
@@ -1,48 +1,30 @@
 <?php
 /* @var $this yii\web\View */
 //$this->title = 'My Yii Application';
+use yii\helpers\Html;
 ?>
 <div class="site-index">
     <div class="jumbotron">
-        <h1>Congratulations!2</h1>
+        <h1>Felicitaciones!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Radiografía Digital de Imagenes Médicas les da la Bienvenida (RDMI).</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h2>Heading</h2>
+            <p>En estos ultimos años hemos ofrecido soluciones de radiografía médica de última 
+               generación a proveedores sanitarios de todo el mundo. Ofrecemos más opciones de equipos de rayos X, 
+               desde radiografía general hasta aplicaciones de consultas especializadas, tales como mamografía y 
+               otorrinolaringología. Nuestros sistemas flexibles de radiografía digital, radiografía computarizada 
+               e impresión médica y de película solucionan los retos de flujo de trabajo, presupuesto y espacio, a 
+               la vez reducen los tiempos de los procedimientos. </p>
+            
+            <img src="<?= Html::encode($directoryAsset . "/img/logos/logov_".Yii::$app->language.".png") ?>" alt="logo" />
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-lg-6">
+                
             </div>
         </div>
 
